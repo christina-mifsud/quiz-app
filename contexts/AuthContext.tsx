21f3eb3c710e6215c14be05ae8b23a39e3e22426
christina-mifsud <christina.mifsud@gmail.com>
2023-12-22 15:26:44 +0000

    Create AuthContext.tsx

---
+++ contexts/AuthContext.tsx
@@ -0,0 +1,50 @@
+"use client";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  ReactNode,
+} from "react";
+import { auth } from "@/firebase/config";
+import { Auth, onAuthStateChanged, User } from "firebase/auth";
+
+///////// HELP!! do these go here or in a separate file with types?
+interface AuthContextValue {
+  currentUser: User | null | undefined;
+  setCurrentUser: (user: User | null | undefined) => void;
+}
+interface AuthProviderProps {
+  children: ReactNode;
+}
+//////////
+
+const AuthContext = createContext<AuthContextValue | undefined>(undefined);
+
+// useAuth Hook - person requesting access to AuthProvider (manager) instead of directly managing the authentication state (going directly for access)
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+// AuthProvider Component - manager of access - keeping track of who has access/is logged in etc through useAuth Hook.
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [currentUser, setCurrentUser] = useState<User | null | undefined>();
+  const [loading, setLoading] = useState(true);
+
+  ////// HELP!! What is happening here?
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
+
+  const value: AuthContextValue = { currentUser, setCurrentUser };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
