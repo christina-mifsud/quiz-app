92c91062e9b5d2bd20a2cae18e6b24a922820c60
christina-mifsud <christina.mifsud@gmail.com>
2023-12-22 16:02:31 +0000

    fix type error

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -3,6 +3,8 @@ import { useRef, useState, MouseEvent } from "react";
 import { useRouter } from "next/navigation";
 import { useSignup } from "@/hooks/useSignup";
 
+// react hook forms
+
 const SignUpPage = () => {
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
@@ -14,11 +16,13 @@ const SignUpPage = () => {
   async function handleSignUp(event: MouseEvent) {
     event.preventDefault();
 
-    if (passwordRef.current?.value !== passwordConfirmRef.current?.value) {
-      return setError("Passwords do not match");
-    } else {
-      await signup(emailRef.current?.value, passwordRef.current?.value);
-      router.push("/");
+    if (emailRef.current?.value && passwordRef.current?.value) {
+      if (passwordRef.current?.value !== passwordConfirmRef.current?.value) {
+        setError("Passwords do not match!");
+      } else {
+        await signup(emailRef.current?.value, passwordRef.current?.value);
+        router.push("/");
+      }
     }
   }
 
