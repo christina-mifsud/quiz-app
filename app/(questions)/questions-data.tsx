4a0d90c0d71e963c67420a22137f5eef1e067ee7
christina-mifsud <christina.mifsud@gmail.com>
2024-01-11 23:45:40 +0000

    Create questions-data.tsx

---
+++ app/(questions)/questions-data.tsx
@@ -0,0 +1,24 @@
+// Questions | multiple choice | single correct answer
+
+export const quiz = {
+  topic: "Fruit",
+  totalQuestions: 3,
+  perQuestionScore: 1,
+  questions: [
+    {
+      question: "What colour is a banana?",
+      choices: ["blue", "red", "yellow"],
+      correctAnswer: "yellow",
+    },
+    {
+      question: "What colour is a blueberry",
+      choices: ["yellow", "blue", "red"],
+      correctAnswer: "blue",
+    },
+    {
+      question: "What colour is a strawberry?",
+      choices: ["red", "yellow", "blue"],
+      correctAnswer: "red",
+    },
+  ],
+};
