e37c532aef4a680aeace97e3606a1bb744192253
christina-mifsud <christina.mifsud@gmail.com>
2024-01-11 18:51:18 +0000

    auth styling

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,10 +1,11 @@
 import SignInForm from "@/components/SignInForm";
+import "@/app/auth.scss";
 
 export default async function SignInPage() {
   return (
-    <div className="card-container">
-      <div className="card">
-        <h1 className="text-white text-2xl mb-5">Sign In</h1>
+    <div className="auth-container">
+      <div className="auth-card">
+        <h1>Sign In</h1>
         <SignInForm />
       </div>
     </div>
