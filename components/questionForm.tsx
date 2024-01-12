54290288d495f2859bb828b84dd650c0485b8d03
christina-mifsud <christina.mifsud@gmail.com>
2024-01-12 13:17:06 +0000

    Mapping choices & next btn

--- components/questionForm.tsx
+++ components/questionForm.tsx
@@ -6,14 +6,27 @@ import { quiz } from "@/app/(questions)/questions-data";
 
 const QuestionForm = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState("");
 
   const router = useRouter();
-  const { questions } = quiz;
+  const { questions } = quiz; // getting single question object from questions objects
+  const { question, choices } = questions[currentQuestion]; // destructuring single question object to access keys & values
+
+  function handleClickNext() {
+    setCurrentQuestion((prev) => prev + 1);
+  }
 
   return (
     <>
       <h3>Question</h3>
       <p>{questions[currentQuestion].question}</p>
+      <ul>
+        {choices.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+
+      <button onClick={(event: MouseEvent) => handleClickNext()}>NEXT</button>
     </>
   );
 };
