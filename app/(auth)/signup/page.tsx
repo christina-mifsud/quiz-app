b847ceef1fea916fb2989bab7bdb5d110a6d3659
christina-mifsud <christina.mifsud@gmail.com>
2024-01-12 17:09:47 +0000

    push styles

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,12 +1,20 @@
 import SignUpForm from "@/components/SignUpForm";
+import "@/app/(auth)/auth.scss";
 
 export default async function SignUpPage() {
   return (
-    <div className="card-container">
-      <div className="card">
-        <h1 className="text-white text-2xl mb-5">Sign Up</h1>
+    <div className="auth-container">
+      <div className="auth-card">
+        <h1>Sign Up</h1>
         <SignUpForm />
       </div>
     </div>
   );
 }
+
+//////////////////////////////////
+
+///// to do
+/// sign out component
+/// css
+/// 3 quiz pages - flow
