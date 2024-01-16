4d1416db228bc293e26f93013b75dc75904b130f
christina-mifsud <christina.mifsud@gmail.com>
2024-01-16 22:24:21 +0000

    highlight selected answer

--- app/(questions)/quiz/page.tsx
+++ app/(questions)/quiz/page.tsx
@@ -1,11 +1,8 @@
 import QuestionForm from "@/components/questionForm";
 import "@/styles/quiz.scss";
-// import "@/styles/auth.scss";
 
 export default async function QuestionPage() {
   return (
-    // <div className="auth-container">
-    //   <div className="auth-card">
     <div className="quiz-container">
       <div className="quiz-card">
         <h1>Fruit Quiz</h1>
--- components/questionForm.tsx
+++ components/questionForm.tsx
@@ -7,6 +7,7 @@ import { quiz } from "@/app/(questions)/questions-data";
 const QuestionForm = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
 
   const router = useRouter();
   const { questions } = quiz; // getting single question object from questions objects
@@ -15,10 +16,13 @@ const QuestionForm = () => {
   function handleClickNext() {
     // reset to null so as not to affect next question
     setSelectedAnswer(null);
+    setSelectedAnswerIndex(null);
     setCurrentQuestion((prevQuestion) => prevQuestion + 1);
   }
 
-  function handleSelectedAnswer(answer: string) {
+  function handleSelectedAnswer(answer: string, index: number) {
+    setSelectedAnswerIndex(index);
+
     if (answer === correctAnswer) {
       setSelectedAnswer(true);
       console.log("CORRECT ANS!!");
@@ -35,15 +39,15 @@ const QuestionForm = () => {
       <ul>
         {choices.map((answer, index) => (
           <li
-            onClick={() => handleSelectedAnswer(answer)}
+            onClick={() => handleSelectedAnswer(answer, index)}
             key={answer}
-            className={selectedAnswer === index ? "selected-answer" : null}
+            className={selectedAnswerIndex === index ? "selected-answer" : null}
           >
             {answer}
           </li>
         ))}
       </ul>
-      {/* check is currentQuestion the last question? if so, change button to finish &
+      {/* checking is currentQuestion the last question? if so, change button to finish &
       set selectedAnswerIndex to null again when next/finish is clicked */}
       <button onClick={handleClickNext} disabled={selectedAnswer === null}>
         {currentQuestion === questions.length - 1 ? "Finish" : "Next"}
