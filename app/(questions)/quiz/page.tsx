0faf7c753fa93ebf4c57fd97b87b3e914e4110c3
christina-mifsud <christina.mifsud@gmail.com>
2024-01-31 22:03:19 +0000

    set up QuizCard component

--- app/(questions)/quiz/page.tsx
+++ app/(questions)/quiz/page.tsx
@@ -1,11 +1,24 @@
 import "@/styles/quiz.scss";
 import { firestore } from "@/firebase/admin-config";
 import Link from "next/link";
+import QuizCard from "@/components/quizCard";
 
+// firestore setup
+// - quiz (collection)
+//   - fruit (document)
+//     - questions (collection)
+//       - question1 (document)
+//         - questionText: "What color is a banana?"
+//         - options: ["yellow", "blue", "red"]
+//         - correctAnswer: "yellow"
+
+// fetch all quiz categories from quiz collection (eg. fruit etc.) & map over them
 export async function getAllCategories() {
   const collectionRef = await firestore.collection("quiz").get();
   const collectionData = collectionRef.docs.map((doc) => {
+    console.log(doc.id);
     return {
+      // getting id which is then used for dynamic routing in the <Link>
       id: doc.id,
     };
   });
@@ -13,6 +26,7 @@ export async function getAllCategories() {
   return collectionData;
 }
 
+// fetches catagories data obtained from prev function & renders something based on data
 export default async function QuestionPage() {
   const data = await getAllCategories();
 
@@ -23,11 +37,14 @@ export default async function QuestionPage() {
         {data.length &&
           data.map((category) => (
             <Link
+              // dynamic routing
               href={`/quiz/${category.id}`}
               className="quiz-card"
-              key="category.id"
+              key={category.id}
             >
-              <h3>{category?.id}</h3>
+              <Link href={""}>
+                <QuizCard category={category} />
+              </Link>
             </Link>
           ))}
       </div>
