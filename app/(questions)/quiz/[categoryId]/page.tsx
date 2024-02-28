d66915c6bc8dc5fbc5b904e94d9488d887a8eaff
christina-mifsud <christina.mifsud@gmail.com>
2024-02-28 21:35:50 +0000

    updated question dynamic routing

--- app/(questions)/quiz/[categoryId]/page.tsx
+++ app/(questions)/quiz/[categoryId]/page.tsx
@@ -1,6 +1,8 @@
+import "@/styles/quiz.scss";
 import { firestore } from "@/firebase/admin-config";
+import Link from "next/link";
 
-// fetch all quiz data from selected quiz collection (eg. fruit etc.) & map over them
+// fetch all questions from selected quiz collection (eg. fruit etc.) & map over them
 export async function fetchQuizData(categoryId) {
   const quizRef = await firestore
     .collection("quiz")
@@ -29,17 +31,19 @@ export default async function QuizPage({ params }) {
   return (
     <div className="quiz-container">
       <h1>Quiz Category: {categoryId}</h1>
-      <div className="quiz-questions">
+      <div className="quiz-cards">
         {fetchedQuizData.length > 0 &&
           fetchedQuizData.map((question) => (
-            <div key={question.id}>
-              <h3>{question.question}</h3>
-              <ul>
-                {question.answers.map((answer, index) => (
-                  <li key={index}>{answer}</li>
-                ))}
-              </ul>
-            </div>
+            <Link
+              legacyBehavior
+              // dynamic routing
+              href={`/quiz/${categoryId}/${question.id}`}
+              key={question.id}
+            >
+              <a className="quiz-card" key={question.id}>
+                <h3>{question?.question}</h3>
+              </a>
+            </Link>
           ))}
       </div>
     </div>
