758c19ba744f70f115346268a2763d9688138ae3
christina-mifsud <christina.mifsud@gmail.com>
2024-04-05 00:13:46 +0100

    calculate results logic - help

--- app/(questions)/quiz/[categoryId]/[questionId]/page.tsx
+++ app/(questions)/quiz/[categoryId]/[questionId]/page.tsx
@@ -1,9 +1,9 @@
-export const revalidate = 120;
+// export const revalidate = 120; // https://react.dev/reference/react/cache#noun-labs-1201738-(2)
 
 import "@/styles/quiz.scss";
 import { fetchDocumentFromFirestore } from "@/data/firestore";
-import { db } from "@/firebase/config";
 import { firestore } from "@/firebase/admin-config";
+import QuestionForm from "@/components/questionForm";
 
 export type QuestionPageProps = {
   params: {
@@ -37,7 +37,6 @@ async function fetchQuestionData(categoryId: string, questionId: string) {
     `quiz/${categoryId}/questions`,
     questionId
   );
-
   return fetchedAnswerData;
 }
 
@@ -45,21 +44,24 @@ export default async function QuestionPage({ params }: QuestionPageProps) {
   const { categoryId, questionId } = params;
   const fetchedAnswerData = await fetchQuestionData(categoryId, questionId);
 
+  console.log("Single Question (FetchedAnswerDataaaaa):", fetchedAnswerData);
+
+  const question = {
+    question: fetchedAnswerData?.question,
+    choices: fetchedAnswerData?.answers,
+    correctAns: fetchedAnswerData?.correctAns,
+  };
+
   return (
     <div className="quiz-container">
+      {/* I am single question card with buttons for answers */}
       <h1>Question: {questionId}</h1>
       <div className="question-card">
-        <h3>{fetchedAnswerData?.question}</h3>
-        <div className="answers">
-          {fetchedAnswerData?.answers.length > 0 &&
-            fetchedAnswerData?.answers.map((answer: any) => (
-              // I am single question card with buttons for answers
-              <button key={answer.id} className="btn">
-                {answer}
-              </button>
-            ))}
-        </div>
+        {question && <QuestionForm questions={[question]} />}
       </div>
     </div>
   );
 }
+
+// as QuestionPage is server-side and we'd like to keep it that way, we are fetching the data from Firestore as normal during server-side rendering & passing said data as props to the questionForm client-side component to handle the results logic etc.
+// (logic here kept breaking everything - so no thanks)
--- components/questionForm.tsx
+++ components/questionForm.tsx
@@ -1,81 +1,88 @@
-import { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { firestore } from "@/firebase/admin-config";
+// as QuestionPage is server-side and we'd like to keep it that way, we are fetching the data from Firestore as normal during server-side rendering & passing said data as props to this client-side component to handle the results logic etc.
+"use client";
 
-///// replace with useEffect to get info from db
-// import { quiz } from "@/app/(questions)/questions-data";
+import { useState } from "react";
 
-const QuestionForm = () => {
-  const [currentQuestion, setCurrentQuestion] = useState(0);
+const QuestionForm = ({ questions }) => {
+  const [activeQuestion, setActiveQuestion] = useState(0);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
-  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
-  const [questions, setQuestions] = useState([]);
+  const [showResult, setShowResult] = useState(false);
 
-  const router = useRouter();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const category = router.query.categoryId;
-        const questionsRef = await firestore
-          .collection("quiz")
-          .doc(category)
-          .collection("questions")
-          .get();
-
-        const questionsData = questionsRef.docs.map((doc) => doc.data());
-        setQuestions(questionsData);
-      } catch (error) {
-        console.log("ERROR!! fetching from Firestore:", error);
-      }
-    };
-    fetchData();
-  }, [router.query.categoryId]);
+  const [result, setResult] = useState({
+    score: 0,
+    correctAnswers: 0,
+    wrongAnswers: 0,
+  });
 
   // const { questions } = quiz; // getting single question object from questions objects
-  const { question, options, correctAns } = questions[currentQuestion]; // destructuring single question object to access keys & values
+  const { question, choices, correctAnswer } = questions[activeQuestion]; // destructuring single question object to access keys & values
 
-  function handleClickNext() {
-    // reset to null so as not to affect next question
+  const onClickNext = () => {
+    setResult((prev) =>
+      selectedAnswer === correctAnswer
+        ? {
+            ...prev,
+            score: prev.score + 5,
+            correctAnswers: prev.correctAnswers + 1,
+          }
+        : { ...prev, wrongAnswers: prev.wrongAnswers + 1 }
+    );
+
     setSelectedAnswer(null);
-    setSelectedAnswerIndex(null);
-    setCurrentQuestion((prevQuestion) => prevQuestion + 1);
-  }
-
-  function handleSelectedAnswer(answer: string, index: number) {
-    setSelectedAnswerIndex(index);
-
-    if (answer === correctAns) {
-      setSelectedAnswer(true);
-      console.log("CORRECT ANS!!");
+    if (activeQuestion !== questions.length - 1) {
+      setActiveQuestion((prev) => prev + 1);
     } else {
-      setSelectedAnswer(false);
-      console.log("WRONGGGG!!");
+      setActiveQuestion(0);
+      setShowResult(true);
     }
-  }
+  };
+
+  const onAnswerSelected = (answer) => {
+    setSelectedAnswer(answer);
+  };
 
   return (
-    <>
-      <p>{question}</p>
-      <ul>
-        {options.map((answer, index) => (
-          <div
-            onClick={() => handleSelectedAnswer(answer, index)}
-            key={answer}
-            className={selectedAnswerIndex === index ? "selected-answer" : null}
-          >
-            <label>{answer}</label>
-            <input type="radio" name="" value="" />
+    <div className="quiz-container">
+      {!showResult ? (
+        <div>
+          <h2>{question}</h2>
+          <ul>
+            {choices.map((answer) => (
+              <li
+                onClick={() => onAnswerSelected(answer)}
+                key={answer}
+                // checking if each li is selected or not
+                className={selectedAnswer === answer ? "selected-answer" : null}
+              >
+                {answer}
+              </li>
+            ))}
+          </ul>
+          <div className="flex-right">
+            <button onClick={onClickNext} disabled={!selectedAnswer}>
+              {activeQuestion === questions.length - 1 ? "Finish" : "Next"}
+            </button>
           </div>
-        ))}
-      </ul>
-
-      {/* checking is currentQuestion the last question? if so, change button to finish &
-      set selectedAnswerIndex to null again when next/finish is clicked */}
-      <button onClick={handleClickNext} disabled={selectedAnswer === null}>
-        {currentQuestion === questions.length - 1 ? "Finish" : "Next"}
-      </button>
-    </>
+        </div>
+      ) : (
+        <div className="result">
+          <h3>Result</h3>
+          <p>
+            Total Question: <span>{questions.length}</span>
+          </p>
+          <p>
+            Total Score:<span> {result.score}</span>
+          </p>
+          <p>
+            Correct Answers:<span> {result.correctAnswers}</span>
+          </p>
+          <p>
+            Wrong Answers:<span> {result.wrongAnswers}</span>
+          </p>
+        </div>
+      )}
+    </div>
   );
 };
+
 export default QuestionForm;
