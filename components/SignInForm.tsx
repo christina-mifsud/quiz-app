57af48f87ab225d93de70c72c6809929a7afc2ed
panosVegas <panos.mita@leovegas.com>
2024-07-13 15:32:51 +0100

    feat(cookie-authentication): implementing cookie authentication post signInWithEmailAndPassword

--- components/SignInForm.tsx
+++ components/SignInForm.tsx
@@ -3,6 +3,7 @@
 import { useRef, useState, MouseEvent } from "react";
 import { useRouter } from "next/navigation";
 import { useSignin } from "@/hooks/useSignin";
+import { auth } from "@/firebase/config";
 
 const SignInForm = () => {
   const emailRef = useRef<HTMLInputElement>(null);
@@ -13,12 +14,27 @@ const SignInForm = () => {
 
   async function handleSignIn(event: MouseEvent) {
     event.preventDefault();
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value
 
-    if (emailRef.current?.value && passwordRef.current?.value) {
-      await signin(emailRef.current?.value, passwordRef.current?.value);
+    if (email && password) {
+      const authResponse = await auth.signInWithEmailAndPassword(email, password);
+
+      if (!authResponse.user) {
+        throw new Error('User not found')
+      }
+
+      const idToken = await authResponse.user.getIdToken();
+
+      const response = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ idToken })
+      });
+
       router.push("/quiz");
-    } else {
-      setError("Wrong Password!");
     }
   }
 
