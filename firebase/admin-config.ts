f85bc24c1b9d50ff6e5058e11e77f85919d056bd
panosVegas <panos.mita@leovegas.com>
2024-07-26 16:38:05 +0100

    chore: evn vairables

--- firebase/admin-config.ts
+++ firebase/admin-config.ts
@@ -5,9 +5,9 @@ import admin from "firebase-admin";
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      client_email: process.env.FIRESTORE_CLIENT_EMAIL,
-      private_key: process.env.FIRESTORE_PRIVATE_KEY,
-      project_id: process.env.FIRESTORE_PROJECT_ID,
+      client_email: process.env.NEXT_PUBLIC_FIRESTORE_CLIENT_EMAIL,
+      private_key: process.env.NEXT_PUBLIC_FIRESTORE_PRIVATE_KEY,
+      project_id: process.env.NEXT_PUBLIC_FIRESTORE_PROJECT_ID,
     } as admin.ServiceAccount),
     databaseURL: "",
   });
