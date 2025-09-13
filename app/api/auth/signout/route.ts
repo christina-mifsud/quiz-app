9f2880b82736053078ffe08e567b3e035707bc76
ChristinaMifsud <102205880+christina-mifsud@users.noreply.github.com>
2025-09-13 21:38:30 +0200

    added signout route

--- app/api/auth/signout/route.ts
+++ app/api/auth/signout/route.ts
@@ -1,1 +1,7 @@
 // DELETE request to remove cookies 
+import { cookies } from "next/headers";
+
+export async function POST() {
+  cookies().delete("__sessionTest");
+  return new Response("Session Deleted", { status: 200 });
+}
